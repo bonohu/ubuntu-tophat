d78bdf403f3d29c85e86b38f4acbd9a8d417e074
Hidemasa Bono <bonohu@gmail.com>
2015-02-04 16:12:16 +0900

    sequence type changed to bz2 format.

--- tophat.sh
+++ tophat.sh
@@ -3,6 +3,6 @@
 wget -O db.zip ftp://ftp.ccb.jhu.edu/pub/data/bowtie_indexes/$1.ebwt.zip
 unzip db.zip
 # get run files
-wget -O seq.fastq $2
+wget -O seq.fastq.bz2 $2
 # run tophat with parameters
-tophat $1 seq.fastq
+tophat $1 seq.fastq.bz2
