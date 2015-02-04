317d7eab7fa4fd2cf7137fe0f0f6a4a841274f30
Hidemasa Bono <bonohu@gmail.com>
2015-02-04 17:51:12 +0900

    fixed to download bowtie2 index

--- tophat.sh
+++ tophat.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # get reference genome
-wget -O db.zip ftp://ftp.ccb.jhu.edu/pub/data/bowtie_indexes/$1.ebwt.zip
+wget -O db.zip ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/$1.zip
 unzip db.zip
 # get run files
 wget -O seq.fastq.bz2 $2
