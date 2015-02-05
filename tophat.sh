e3dce6be37efb984cea3798e6c45b13de27e207d
Hidemasa Bono <bonohu@gmail.com>
2015-02-05 16:23:03 +0900

    add rm command for reducing disk space.

--- tophat.sh
+++ tophat.sh
@@ -2,8 +2,10 @@
 # get reference genome
 wget -O db.zip ftp://ftp.ccb.jhu.edu/pub/data/bowtie2_indexes/$1.zip
 unzip db.zip
+rm db.zip
 # get run files
 wget -O seq.fastq.bz2 $2
 bzip2 -cd seq.fastq.bz2 > seq.fastq
+rm seq.fastq.bz2
 # run tophat with parameters
 tophat $1 seq.fastq
